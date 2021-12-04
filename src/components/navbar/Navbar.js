15364a88948ef5722c2de51af68d02be4f38f9b9
DMTBusra <gencbusra4325@gmail.com>
2021-12-04 17:39:24 +0300

    first commit

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,15 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+    return(
+        <div className="navbar">
+            <a href="aboutus">ABOUT US</a>
+            <a href="FORYOU">FOR YOU</a>
+            <a href="SERVİCES">SERVİCES</a>
+            <a href="BLOG">BLOG</a>
+            <a href="VLOG">VLOG</a>
+            <a href="contact">CONTACT</a>
+        </div>
+    )
+}
+export default Navbar;
